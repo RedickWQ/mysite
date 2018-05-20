3926aed4b8a84e2a1dfd36ea243bd0556537fb54
Paul.wang <405605830@qq.com>
2018-05-20 22:23:27 +0800

    add index.html

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 
 
 def login(request):
-    return render(request, "login.html")
+    error_msg = ""
 
-def home(request):
-    return HttpResponse("OK")
+    if request.method == "GET":
+        return render(request, "login.html")
+    else:
+        username = request.POST.get("username")
+        pwd = request.POST.get("pwd")
+        if username == "root" and pwd == "123":
+            return redirect("/index")
+        else:
+            error_msg = "invalid user name or password"
+    return render(request, "login.html",{"error_msg":error_msg})
+
+
+def index(request):
+    return render(request, "index.html")
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from cmdb import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home),
+    path('index/', views.index),
     path('login/', views.login),
 ]
